0b6f27c062be2f7b321e67a16b4156eee74f662f
usaljuntak14 <sbinusal@gmail.com>
2021-06-19 12:10:14 +0700

    Tahap 4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <HelloWorld />
+  <router-link to="/">Halaman Auth</router-link>
+  &nbsp;
+  <router-link to="/todo">Halaman Todo</router-link>
+  &nbsp;
+  <router-link to="/user">Halaman User</router-link>
+  <router-view></router-view>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+import Todo from './components/Todo.vue'
+import User from './components/User.vue'
 </script>
